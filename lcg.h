b84dcb0a7fc69c4d93ee66c76d4b5bf7103c0f60
Your Name <you@example.com>
2019-05-16 14:55:59 +0200

    stuffies

--- lcg.h
+++ lcg.h
@@ -1,33 +1,30 @@
-#ifndef LCG_H
-#define LCG_H
-
+#ifndef LCH_H
+#define LCH_H
 #include <cstddef>
 #include <cmath>
-
 namespace lcg
 {
-	struct state
-	{
-		std::size_t last;
-		std::size_t a;
-		std::size_t c;
-		std::size_t mod;
-	};
+    struct state{
+        std::size_t last;
+        std::size_t a;
+        std::size_t c;
+        std::size_t mod;
+    };
 
-	std::size_t gen_n(state& s)
-	{
-		return s.last = (s.a * s.last + s.c) % s.mod;
-	}
+    std::size_t gen_n(state & s)
+    {
+        return s.last = (s.a * s.last + s.c) % s.mod;
+    }
+    double normalized_gen(state &s)
+    {
+        return gen_n(s)/((double)s.mod);
+    }
+    template <typename T>
+    T exp_transform(double u, double beta)
+    {
+        double lambda = 1/beta;
+        return ((-lambda) * log(1-u));
+    }
 
-	double norm_gen(state& s)
-	{
-		return gen_n(s) / (double)s.mod;
-	}
-
-	double exp_transform(double u, double mean)
-	{
-		return -(mean) * std::log(1 - u);
-	}
 }
-
 #endif
