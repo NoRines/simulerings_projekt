8de37fd04f2ac1a49cea0d304ffb2cc380e4efe2
NoRines <markus.falt@gmail.com>
2019-06-12 15:35:39 +0200

    jag är kung

--- CSMApartProject.cc
+++ CSMApartProject.cc
@@ -6,15 +6,14 @@
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
 #include "ns3/traffic-control-module.h"
-#include "ns3/flow-monitor-module.h"
+#include "ns3/csma-module.h"
 
 
-#include "lcg.h"
+//#include "lcg.h"
 
 using namespace ns3;
 
-
-void TcPacketsInQueue (QueueDiscContainer qdiscs, Ptr<OutputStreamWrapper> stream)
+void TcPacketsInQueueCSMA (QueueDiscContainer qdiscs, Ptr<OutputStreamWrapper> stream)
 {
 	//Get current queue size value and save to file.
 	*stream->GetStream () << Simulator::Now().GetSeconds();
@@ -27,22 +26,13 @@ void TcPacketsInQueue (QueueDiscContainer qdiscs, Ptr<OutputStreamWrapper> strea
 	*stream->GetStream() << std::endl;
 }
 
-static void GenerateTraffic (Ptr<Socket> socket, Ptr<ExponentialRandomVariable> randomSize,	Ptr<ExponentialRandomVariable> randomTime)
+static void GenerateTrafficCSMA (Ptr<Socket> socket, Ptr<ExponentialRandomVariable> randomSize,	Ptr<ExponentialRandomVariable> randomTime)
 {
 	uint32_t pktSize = randomSize->GetInteger (); //Get random value for packet size
 	socket->Send (Create<Packet> (pktSize));
 
 	Time pktInterval = Seconds(randomTime->GetValue ()); //Get random value for next packet generation time 
-	Simulator::Schedule (pktInterval, &GenerateTraffic, socket, randomSize, randomTime); //Schedule next packet generation
-}
-
-static void MyGenerateTraffic (Ptr<Socket> socket, lcg::state& randomSize, lcg::state& randomTime, double interarivalTime)
-{
-	uint32_t pktSize = (uint32_t)lcg::exp_transform(norm_gen(randomSize), 70);
-	socket->Send (Create<Packet> (pktSize));
-
-	Time pktInterval = Seconds(lcg::exp_transform(norm_gen(randomTime), interarivalTime));
-	Simulator::Schedule (pktInterval, &MyGenerateTraffic, socket, randomSize, randomTime, interarivalTime); //Schedule next packet generation
+	Simulator::Schedule (pktInterval, &GenerateTrafficCSMA, socket, randomSize, randomTime); //Schedule next packet generation
 }
 
 
@@ -55,14 +45,11 @@ int main(int argc, char** argv)
 	//LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
 	//LogComponentEnable("MyUdpEchoServerApplication", LOG_LEVEL_INFO);
 	
-	// Change these after every run...
-	uint32_t seed1 = 13852;
-	uint32_t seed2 = 3159;
-	
-	RngSeedManager::SetSeed(seed1 % seed2);
+	// Ändra dessa i varje run
+	RngSeedManager::SetSeed(25626);
 	RngSeedManager::SetRun(6546);
 	
-	double simTime = 6.0;
+	double simTime = 31.0;
 
 	NodeContainer container;
 	container.Create(9);
@@ -75,10 +62,27 @@ int main(int argc, char** argv)
 	NodeContainer ncDtoG = NodeContainer(container.Get(3), container.Get(6));
 	NodeContainer ncGtoServer = NodeContainer(container.Get(6), container.Get(7));
 	NodeContainer ncGtoRouter = NodeContainer(container.Get(6), container.Get(8));
+	
+	//PointToPointHelper p2p;
+	//p2p.SetQueue ("ns3::DropTailQueue", "MaxSize", StringValue ("1p"));
 
-	PointToPointHelper p2p;
-	p2p.SetQueue ("ns3::DropTailQueue", "MaxSize", StringValue ("1p"));
+	CsmaHelper csma;
+  	csma.SetChannelAttribute ("DataRate", StringValue ("5Mbps"));
+	csma.SetQueue ("ns3::DropTailQueue", "MaxSize", StringValue ("1p"));
+	
+	NetDeviceContainer dcAtoE = csma.Install(ncAtoE);
+	NetDeviceContainer dcEtoG = csma.Install(ncEtoG);
+	NetDeviceContainer dcBtoF = csma.Install(ncBtoF);
+	NetDeviceContainer dcCtoF = csma.Install(ncCtoF);
+	NetDeviceContainer dcDtoG = csma.Install(ncDtoG);
 
+	csma.SetChannelAttribute ("DataRate", StringValue ("8Mbps"));
+	NetDeviceContainer dcFtoG = csma.Install(ncFtoG);
+	NetDeviceContainer dcGtoRouter = csma.Install(ncGtoRouter);
+
+	csma.SetChannelAttribute ("DataRate", StringValue ("10Mbps"));
+	NetDeviceContainer dcGtoServer = csma.Install(ncGtoServer);
+	/*
 	p2p.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
 	NetDeviceContainer dcAtoE = p2p.Install(ncAtoE);
 	NetDeviceContainer dcEtoG = p2p.Install(ncEtoG);
@@ -92,12 +96,16 @@ int main(int argc, char** argv)
 
 	p2p.SetDeviceAttribute("DataRate", StringValue("10Mbps"));
 	NetDeviceContainer dcGtoServer = p2p.Install(ncGtoServer);
+	*/
+	
 
+	
 	InternetStackHelper internet;
 	internet.Install(container);
+	
 
 	TrafficControlHelper tch;
-	tch.SetRootQueueDisc("ns3::FifoQueueDisc", "MaxSize", StringValue("1000p"));
+	tch.SetRootQueueDisc("ns3::FifoQueueDisc", "MaxSize", StringValue("100000p"));
 	
 	QueueDiscContainer qdiscs = tch.Install(dcAtoE);
 	qdiscs.Add(tch.Install(dcEtoG));
@@ -115,9 +123,9 @@ int main(int argc, char** argv)
 		<< "BfromF," << "FfromB," << "CfromF," << "FfromC," << "DfromG," << "GfromD,"
 		<< "FfromG," << "GfromF," << "GfromRouter," << "RouterFromG," << "GfromServer," << "ServerFromG" << std::endl;
 
-	for(float t = 1.0f; t < simTime; t+=0.00001f)
+	for(float t = 1.0f; t < simTime; t+=0.0001f)
 	{
-		Simulator::Schedule(Seconds(t), &TcPacketsInQueue, qdiscs, stream);
+		Simulator::Schedule(Seconds(t), &TcPacketsInQueueCSMA, qdiscs, stream);
 	}
 
 
@@ -153,60 +161,85 @@ int main(int argc, char** argv)
 	double starttime = 1.0;
 	double stoptime = simTime;
 	ApplicationContainer serverApp = serverHelper.Install(ncGtoServer.Get(1));
-	serverApp.Start(Seconds(starttime));
+	serverApp.Start(Seconds(1.0));
 	serverApp.Stop(Seconds(stoptime));
 
 
 	PacketSinkHelper packetSinkHelper("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), 300));
 	ApplicationContainer packetSinkApp = packetSinkHelper.Install(ncGtoRouter.Get(1));
-	packetSinkApp.Start(Seconds(starttime));
+	packetSinkApp.Start(Seconds(1.0));
 	packetSinkApp.Stop(Seconds(stoptime));
+
+	PacketSinkHelper packetSinkHelperA("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), 300));
+	ApplicationContainer packetSinkAppA = packetSinkHelperA.Install(ncAtoE.Get(0));
+	packetSinkAppA.Start(Seconds(1.0));
+	packetSinkAppA.Stop(Seconds(stoptime));
+
+	PacketSinkHelper packetSinkHelperB("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), 300));
+	ApplicationContainer packetSinkAppB = packetSinkHelperB.Install(ncBtoF.Get(0));
+	packetSinkAppB.Start(Seconds(1.0));
+	packetSinkAppB.Stop(Seconds(stoptime));
+
+	PacketSinkHelper packetSinkHelperC("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), 300));
+	ApplicationContainer packetSinkAppC = packetSinkHelperC.Install(ncCtoF.Get(0));
+	packetSinkAppC.Start(Seconds(1.0));
+	packetSinkAppC.Stop(Seconds(stoptime));
+
+	PacketSinkHelper packetSinkHelperD("ns3::UdpSocketFactory", InetSocketAddress(Ipv4Address::GetAny(), 300));
+	ApplicationContainer packetSinkAppD = packetSinkHelperD.Install(ncDtoG.Get(0));
+	packetSinkAppD.Start(Seconds(1.0));
+	packetSinkAppD.Stop(Seconds(stoptime));
+
 
 
 	TypeId tid = TypeId::LookupByName ("ns3::UdpSocketFactory");
 
 
-	auto DefThing = [&tid, starttime, seed1, seed2](Ptr<Node> node, Ipv4Address serverAddress, double meanInterTime, double meanSize)
+	auto DefThing = [&tid, starttime](Ptr<Node> node, Ipv4Address serverAddress, double meanInterTime, double meanSize)
 	{
 		Ptr<Socket> sourceA = Socket::CreateSocket(node, tid);
 		sourceA->Connect (InetSocketAddress (serverAddress, 9));
 	
-		//double mean = meanInterTime;
-		//Ptr<ExponentialRandomVariable> randomTime = CreateObject<ExponentialRandomVariable> ();
-		//randomTime->SetAttribute ("Mean", DoubleValue (mean));
+		double mean = meanInterTime;
+		Ptr<ExponentialRandomVariable> randomTime = CreateObject<ExponentialRandomVariable> ();
+		randomTime->SetAttribute ("Mean", DoubleValue (mean));
 	
-		//mean = meanSize;
-		//Ptr<ExponentialRandomVariable> randomSize = CreateObject<ExponentialRandomVariable> ();
-		//randomSize->SetAttribute ("Mean", DoubleValue (mean));
-		//
-		lcg::state randomSize = {seed1, 22695477, 1, (unsigned int)(1 << 31)};
-		lcg::state randomTime = {seed2, 22695477, 1, (unsigned int)(1 << 31)};
+		//mean = (bitRate/(8*4800)-30); // (1 000 000 [b/s])/(8 [b/B] * packet service rate [1/s]) - 30 [B (header bytes)]
+		//std::cout << mean << std::endl;
+		mean = meanSize;
+		Ptr<ExponentialRandomVariable> randomSize = CreateObject<ExponentialRandomVariable> ();
+		randomSize->SetAttribute ("Mean", DoubleValue (mean));
 
-		Simulator::ScheduleWithContext (sourceA->GetNode()->GetId(), Seconds (starttime), &MyGenerateTraffic, sourceA, randomSize, randomTime, meanInterTime);
-		//Simulator::ScheduleWithContext (sourceA->GetNode()->GetId(), Seconds (starttime), &GenerateTraffic, sourceA, randomSize, randomTime);
+		Simulator::ScheduleWithContext (sourceA->GetNode()->GetId(), Seconds (starttime), &GenerateTrafficCSMA, sourceA, randomSize, randomTime);
 	};
 	
-	DefThing(ncAtoE.Get(0), iGtoServer.GetAddress(1), 0.002, 70);
-	DefThing(ncBtoF.Get(0), iGtoServer.GetAddress(1), 0.002, 70);
-	DefThing(ncCtoF.Get(0), iGtoServer.GetAddress(1), 0.0005, 70);
-	DefThing(ncDtoG.Get(0), iGtoServer.GetAddress(1), 0.001, 70);
+	DefThing(ncAtoE.Get(0), iGtoServer.GetAddress(1), 0.002, 100);
+	DefThing(ncBtoF.Get(0), iGtoServer.GetAddress(1), 0.002, 100);
+	DefThing(ncCtoF.Get(0), iGtoServer.GetAddress(1), 0.0005, 100);
+	DefThing(ncDtoG.Get(0), iGtoServer.GetAddress(1), 0.001, 100);
 
 	Ipv4GlobalRoutingHelper::PopulateRoutingTables();
 
-	FlowMonitorHelper flowmon;
+	//FlowMonitorHelper flowmon;
 	//Ptr<FlowMonitor> monitor = flowmon.InstallAll();
-	Ptr<FlowMonitor> monitor = flowmon.Install(container);
-
-	p2p.EnablePcap("proj_router", dcGtoRouter.Get(1), true);
-	p2p.EnablePcap("proj_server", dcGtoServer.Get(1), true);
-	p2p.EnablePcap("proj_f", dcFtoG.Get(0), true);
-	p2p.EnablePcap("proj_g", dcFtoG.Get(1), true);
+	//p2p.EnablePcapAll("scratch/all_da_packets",true);
+	//p2p.EnablePcap("proj_router", dcGtoRouter.Get(1), true);
+	//p2p.EnablePcap("proj_server", dcGtoServer.Get(1), true);
+	//p2p.EnablePcap("proj_g", dcGtoServer.Get(0), true);
+	csma.EnablePcapAll("scratch/all_da_packets",true);
+	csma.EnablePcap("proj_router", dcGtoRouter.Get(1), true);
+	csma.EnablePcap("proj_server", dcGtoServer.Get(1), true);
+	csma.EnablePcap("proj_g", dcGtoServer.Get(0), true);
+	csma.EnablePcap("proj_a", dcAtoE.Get(0), true);
 	Simulator::Stop(Seconds(simTime));
 	Simulator::Run();
 
-	flowmon.SerializeToXmlFile("flowfilestatscool.xml", false, true);
+	//Ptr<Ipv4FlowClassifier> classifier = DynamicCast<Ipv4FlowClassifier>(flowmon.GetClassifier());
+	//std::map<FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats();
+
 
 	Simulator::Destroy();
-
+	std::cout<<std::endl;
+	
 	return 0;
 }
